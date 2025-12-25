e1ca23e836f55de64eb2bee653c97645b2941133
byebydoggy <byebydoggy@tg.un>
2025-12-26 02:47:03 +0800

    update ABBC exp

---
+++ src/test/constants/bsc.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+address constant USDT = 0x55d398326f99059fF775485246999027B3197955;
+address constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
+address constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
